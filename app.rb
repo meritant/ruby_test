c1649fcc81c837bdaeee153eff56bc2f02e520ea
User User <useruser@192.168.1.112>
2024-05-14 14:18:25 +0300

    Home work done (with errors)

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require 'rubygems'
 require 'sinatra/reloader'
-# require 'pony'
 require 'sqlite3'
+
 
 
 configure do
@@ -16,36 +16,32 @@ configure do
 	"barber"	TEXT NOT NULL DEFAULT "no_one",
 	"color"	TEXT NOT NULL DEFAULT "#ffffff"
   );'
+  db.execute 'CREATE TABLE IF NOT EXISTS
+  "barber" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)'
+
 end
-
-
-
-# @db.execute "select * from user" do |user|
-#   puts user
-#   puts '+'*25
-# end
-# @db.close
 
 get '/' do
   @message = 'Super valuable website!!!'
   erb :index
 end
-#
-# get '/about' do
-#   erb :about
-# end
-#
-# get '/wealth' do
-#   erb :wealth
-# end
-#
+
+# Get for Visit
 get '/visit' do
+
+  # Loading list of barbers from database
+  db = connect_db
+  db.results_as_hash = true
+  @barber_list = db.execute 'select * from barber'
   erb :visit
 end
-#
-# # Code for VISIT page
+
+# POST for VISIT page
 post '/visit' do
-  erb 'Success'
   @username = params[:username]
   @phone = params[:phone]
   @datetime = params[:datetime]
@@ -57,7 +53,6 @@ post '/visit' do
   hh = {username: 'Enter Name', phone: 'Enter phone', datetime: 'Enter time', dresser: 'Enter Dresser'}
 
   # Creating a loop
-
   hh.each_key do |key|
       # Checking if params[] have an empty value
       # if yes, assign error variable a message from the hash
@@ -68,14 +63,12 @@ post '/visit' do
     end
   end
 
-
   # Inserting into table user
   # Using ???? to protect from SQL Injection
   connect_db.execute "insert into user (name, phone, date_stamp, barber, color)
 values (?, ?, ?, ?, ?)", [@username, @phone, @datetime, @dresser, @color]
 
-
-  erb "user is #{@username}, #{@phone}, #{@datetime} and #{@dresser} aand color is #{@color}"
+  erb " Thank you for register:  #{@username}, #{@phone}, #{@datetime} and #{@dresser} aand color is #{@color}"
 end
 # # Visit code END
 
@@ -91,14 +84,12 @@ get '/showusers' do
   erb 'We going to show our users here'
   @db = connect_db
   @db.results_as_hash = true
-  @db.execute 'select * from user' do |row|
-
-   puts row['name']
-   puts row['date_stamp']
-   puts '+' * 25
-  end
-  @db.close
+  @user = @db.execute 'select * from user order by id desc'
+  erb :showusers
 end
+
+
+
 #
 # post '/about' do
 #   require 'pony'
